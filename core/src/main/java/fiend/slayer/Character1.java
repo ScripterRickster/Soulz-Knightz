f9b4b3fffa2e33f88839dac3366a350140bd2fb2
ScripterRickster <101468308+ScripterRickster@users.noreply.github.com>
2024-12-10 14:46:03 -0500

    aaaaaa

--- core/src/main/java/fiend/slayer/Character1.java
+++ core/src/main/java/fiend/slayer/Character1.java
@@ -1,13 +1,16 @@
 package fiend.slayer;
 
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+
 
 class Character1{
 
     Sprite img;
 
-
     public Character1(){
         System.out.println("hihihihihihih");
     }
+
+
 }
--- core/src/main/java/fiend/slayer/Main.java
+++ core/src/main/java/fiend/slayer/Main.java
@@ -1,6 +1,13 @@
 package fiend.slayer;
 
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input.Keys;
+import com.badlogic.gdx.InputProcessor;
+import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.audio.Sound;
+
 
 // test
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
